d6b4093aa29ef8a1afdc8276ee3b2ce9bbe5a7eb
Ilya Savin <ilya.savin@innowise-group.com>
2022-08-02 19:13:53 +0300

    refactor: changed name 'innotter' app to 'blog' app

--- InnotterDjango/InnotterDjango/settings.py
+++ InnotterDjango/InnotterDjango/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'innotter.apps.InnotterConfig',
+    'blog.apps.BlogConfig',
     'user.apps.UserConfig'
 ]
 
--- InnotterDjango/innotter/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class InnotterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'innotter'
-    label = 'innotter'
-    verbose_name = 'Innotter'
