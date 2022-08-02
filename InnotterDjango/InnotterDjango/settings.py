a25b9e8d45a4b9c548fb0e85feaa1c57d229d115
Ilya Savin <ilya.savin@innowise-group.com>
2022-08-02 19:10:01 +0300

    new: created new apps 'innotter' and 'user'

--- InnotterDjango/InnotterDjango/settings.py
+++ InnotterDjango/InnotterDjango/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'innotter.apps.InnotterConfig',
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
