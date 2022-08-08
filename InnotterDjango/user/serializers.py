bf0f60273db22b83113bf336d9dc2a6763510153
Twelve <kana.suzucki@gmail.com>
2022-08-08 13:36:19 +0300

    new: created UserSerializer

---
+++ InnotterDjango/user/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    pages = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True
+    )
+
+    follows = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True
+    )
+
+    requests = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'username', 'password',
+            'role', 'image_s3_path', 'is_blocked',
+            'pages', 'follows', 'requests'
+        ]
+        read_only_fields = [
+            'id', 'pages', 'follows', 'requests'
+        ]
