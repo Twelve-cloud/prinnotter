3322debe544d3c8f916ca636b81649714f184b15
Twelve <kana.suzucki@gmail.com>
2022-08-09 09:04:00 +0300

    new: created registration and login serializers

--- InnotterDjango/user/serializers.py
+++ InnotterDjango/user/serializers.py
@@ -1,14 +1,71 @@
 from rest_framework import serializers
 from user.models import User
+from django.contrib.auth import authenticate
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         max_length=128,
         min_length=8,
         write_only=True
     )
 
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(
+        max_length=255,
+        min_length=8,
+        write_only=True
+    )
+
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError(
+                'An email address is required to log in.'
+            )
+
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+
+        if user.is_blocked:
+            raise serializers.ValidationError(
+                'This user has been blocked.'
+            )
+
+        return {
+            'email': user.email,
+            'username': user.username,
+            'access_token': user.access_token,
+            'refresh_token': user.refresh_token
+        }
+
+
+class UserSerializer(serializers.ModelSerializer):
     pages = serializers.PrimaryKeyRelatedField(
         many=True,
         read_only=True
