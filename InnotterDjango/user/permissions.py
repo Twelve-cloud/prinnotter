06d6fcab5a4219b0b2da78ea6b1117d78f43313f
Twelve <twelve_cloud@gmail.com>
2022-08-10 14:25:55 +0300

    new: created IsUserOwner, IsAdmin, IsModerator, IsUserOwnerOrAdmin permission classes

---
+++ InnotterDjango/user/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+from user.models import User
+
+
+class IsUserOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == User.Roles.ADMIN
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == User.Roles.MODERATOR
+
+
+class IsUserOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return IsAdmin.has_permission(self, request, view) or \
+            IsUserOwner.has_object_permission(self, request, view, obj)
