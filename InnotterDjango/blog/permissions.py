6453be50368e0a94cbde6c20c0d8555fd986540d
Twelve <twelve_cloud@gmail.com>
2022-08-11 10:52:23 +0300

    new: created permissions for blog app

---
+++ InnotterDjango/blog/permissions.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import AnonymousUser
+from rest_framework import permissions
+from blog.models import Page, Post, Tag
+from user.models import User
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, AnonymousUser):
+            return False
+        return request.user.role == User.Roles.ADMIN
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, AnonymousUser):
+            return False
+        return request.user.role == User.Roles.MODERATOR
+
+
+class IsPageOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class IsPageNotPrivate(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if IsPageOwnerOrAdminOrModerator.has_object_permission(self, request, view, obj):
+            return True
+        return not obj.is_private
+
+
+class IsPageNotBlocked(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if IsAdminOrModerator.has_permission(self, request, view):
+            return True
+        return obj.unblock_date is None
+
+
+class IsPageOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return IsPageOwner.has_object_permission(self, request, view, obj) or \
+            IsAdmin.has_permission(self, request, view)
+
+
+class IsPageOwnerOrAdminOrModerator(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return IsPageOwner.has_object_permission(self, request, view, obj) or \
+            IsAdminOrModerator.has_permission(self, request, view)
+
+
+class IsAdminOrModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return IsAdmin.has_permission(self, request, view) or \
+            IsModerator.has_permission(self, request, view)
