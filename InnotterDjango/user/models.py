f414381e201dba6360b13422165e1c276d5c25e0
Twelve <kana.suzucki@gmail.com>
2022-08-13 16:16:57 +0300

    fix: some fixes

--- InnotterDjango/user/models.py
+++ InnotterDjango/user/models.py
@@ -1,40 +1,9 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin
-)
+from django.contrib.auth.models import AbstractBaseUser
 from datetime import datetime, timedelta
+from user.managers import UserManager
 import jwt
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, username, email, password, role):
-        if username is None:
-            raise TypeError('Users must have a username.')
-
-        if email is None:
-            raise TypeError('Users must have an email address.')
-
-        if password is None:
-            raise TypeError('Users must have a password.')
-
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            role=role
-        )
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    def create_superuser(self, username, email, password):
-        user = self.create_user(username, email, password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
-
-        return user
 
 
 class User(AbstractBaseUser):
@@ -47,14 +16,12 @@ class User(AbstractBaseUser):
     email = models.EmailField(
         unique=True,
         verbose_name='Email',
-        db_column='Email'
     )
 
     username = models.CharField(
         max_length=32,
         unique=True,
         verbose_name='Username',
-        db_column='Username'
     )
 
     image_s3_path = models.CharField(
@@ -62,20 +29,17 @@ class User(AbstractBaseUser):
         null=True,
         blank=True,
         verbose_name='PathToImage',
-        db_column='PathToImage'
     )
 
     role = models.CharField(
         max_length=9,
         choices=Roles.choices,
         verbose_name='Rolename',
-        db_column='Rolename'
     )
 
     is_blocked = models.BooleanField(
         default=False,
         verbose_name='Blocked?',
-        db_column='Blocked'
     )
 
     USERNAME_FIELD = 'email'
@@ -92,26 +56,12 @@ class User(AbstractBaseUser):
         return self.username
 
     def get_absolute_url(self):
-        return f'/user/{self.pk}/'
+        return f'/users/{self.pk}/'
 
-    @property
-    def access_token(self):
-        return self._generate_access_token()
-
-    @property
-    def refresh_token(self):
-        return self._generate_refresh_token()
-
-    def _generate_access_token(self):
-        dt = datetime.now() + timedelta(minutes=15)
+    def generate_token(self, *, type):
+        lifetime = timedelta(minutes=15) if type == 'access' else timedelta(days=30)
+        dt = datetime.now() + lifetime
         payload = {'id': self.pk, 'exp': int(dt.strftime('%s'))}
         access_token = jwt.encode(payload, settings.SECRET_KEY, alg='HS256')
 
         return access_token.decode('utf-8')
-
-    def _generate_refresh_token(self):
-        dt = datetime.now() + timedelta(days=1)
-        payload = {'id': self.pk, 'exp': int(dt.strftime('%s'))}
-        refresh_token = jwt.encode(payload, settings.SECRET_KEY, alg='HS256')
-
-        return refresh_token.decode('utf-8')
