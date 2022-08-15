83b3cc0c617190a80d242934ae8f260d3130058b
Twelve <twelve_cloud@gmail.com>
2022-08-15 11:22:34 +0300

    fix: some fixes

--- InnotterDjango/entrypoint.sh
+++ InnotterDjango/entrypoint.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
-poetry run python manage.py makemigrations blog
-poetry run python manage.py makemigrations user
-poetry run python manage.py migrate blog
-poetry run python manage.py migrate user
-poetry run python manage.py makemigrations
+poetry run python manage.py migrate
 poetry run python manage.py runserver 0.0.0.0:8000
