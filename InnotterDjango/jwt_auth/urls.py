ab436618f3a2440c1938871e7f1cd2c6b9bf7a10
Twelve <kana.suzucki@gmail.com>
2022-08-26 19:10:45 +0300

    fix: some fixes

--- InnotterDjango/jwt_auth/urls.py
+++ InnotterDjango/jwt_auth/urls.py
@@ -5,10 +5,6 @@ from jwt_auth import views
 
 router = DefaultRouter()
 
-router.register(
-    prefix='jwt',
-    viewset=views.AuthViewSet,
-    basename='jwt',
-)
+router.register(prefix='jwt', viewset=views.AuthViewSet, basename='jwt')
 
 urlpatterns = router.urls
