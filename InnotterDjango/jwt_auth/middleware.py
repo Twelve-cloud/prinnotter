0e4dbc101a1be8a9c08a310aa724d613f01f5d96
Twelve <twelve_cloud@gmail.com>
2022-08-30 13:18:08 +0300

    refactor: some changes

--- InnotterDjango/jwt_auth/middleware.py
+++ InnotterDjango/jwt_auth/middleware.py
@@ -10,7 +10,7 @@ class JWTMiddleware:
     def __call__(self, request):
         access_token = request.COOKIES.get('access_token', None)
 
-        if access_token:
+        if access_token and request.path != '/auth/jwt/sign_in/' and request.path != '/auth/jwt/refresh/' and request.path != '/admin/' and request.path != '/admin/login/':
             payload = get_payload_by_token(access_token)
 
             if payload is None:
