d9310373ed0a3df71ca9d68946d562711177be46
Twelve <twelve_cloud@gmail.com>
2022-08-30 13:17:53 +0300

    feature: made user block feature

--- InnotterDjango/user/views.py
+++ InnotterDjango/user/views.py
@@ -1,9 +1,12 @@
 from user.permissions import (
-    IsNotAuthentificatedOrAdmin, IsUserOwnerOrAdmin
+    IsNotAuthentificatedOrAdmin, IsUserOwnerOrAdmin, IsAdmin
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from user.serializers import UserSerializer
 from rest_framework import viewsets
+from rest_framework import status
 from user.models import User
 
 
@@ -33,8 +36,20 @@ class UserViewSet(viewsets.ModelViewSet):
             IsAuthenticated,
             IsUserOwnerOrAdmin,
         ),
+        'block': (
+            IsAuthenticated,
+            IsAdmin,
+        ),
     }
 
     def get_permissions(self):
         self.permission_classes = self.permission_map.get(self.action, [])
         return super(self.__class__, self).get_permissions()
+
+    @action(detail=True, methods=['patch'])
+    def block(self, request, pk=None):
+        user = User.objects.get(pk=pk)
+        serializer = self.serializer_class(user, data={}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(is_blocked=request.data.get('is_blocked', False))
+        return Response(serializer.data, status=status.HTTP_200_OK)
