89a97667e37c8d4495e6d38c3f54ac3311b64135
Twelve <kana.suzucki@gmail.com>
2022-09-10 17:22:17 +0300

    added some fixtures for test_models.py

--- InnotterDjango/tests/user_tests/conftest.py
+++ InnotterDjango/tests/user_tests/conftest.py
@@ -1,15 +1,13 @@
 import pytest
+from user.models import User
+from model_bakery import baker
 
 
-users = [
-    {'email': 'ilya@mail.ru', 'username': 'ilya', 'password': '12341234'},
-    {'email': 'artem@mail.ru', 'username': 'artem', 'password': '12341234'},
-    {'email': 'gordey@mail.ru', 'username': 'gordey', 'password': '12341234'}
-]
-
-user_ids = [f"User: {user['username']}" for user in users]
+@pytest.fixture()
+def created_user():
+    return baker.make(User, role='u')
 
 
-@pytest.fixture(params=users, ids=user_ids)
-def new_user(request):
-    return request.param
+@pytest.fixture()
+def prepared_user():
+    return baker.prepare(User, role='u')
