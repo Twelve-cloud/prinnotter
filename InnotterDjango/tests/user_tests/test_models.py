8e89a1a9b99305759606580d2b305c1432b45b7a
Twelve <kana.suzucki@gmail.com>
2022-09-12 01:20:48 +0300

    fix: somes fixes in test_models

--- InnotterDjango/tests/user_tests/test_models.py
+++ InnotterDjango/tests/user_tests/test_models.py
@@ -5,9 +5,10 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
-def test_user_view(created_user):
-    assert created_user.__str__() == f'{created_user.id}: {created_user.username}'
+class TestUserModel:
+    def test_user_view(self, created_user):
+        assert created_user.__str__() == f'{created_user.id}: {created_user.username}'
 
 
-def test_user_get_absolute_url(created_user):
-    assert created_user.get_absolute_url() == f'/users/{created_user.pk}/'
+    def test_user_get_absolute_url(self, created_user):
+        assert created_user.get_absolute_url() == f'/users/{created_user.pk}/'
