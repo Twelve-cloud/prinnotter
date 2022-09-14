720f762bb5517cd595dc01ed45396435ac77c1f6
Twelve <kana.suzucki@gmail.com>
2022-09-14 23:05:35 +0300

    fix: some fixes

--- InnotterDjango/tests/auth_tests/test_middlewares.py
+++ InnotterDjango/tests/auth_tests/test_middlewares.py
@@ -1,6 +1,5 @@
 from rest_framework.exceptions import PermissionDenied, AuthenticationFailed
 from jwt_auth.middleware import JWTMiddleware
-from rest_framework import status
 import pytest
 
 
@@ -8,16 +7,15 @@ pytestmark = pytest.mark.django_db
 
 
 class TestMiddleware:
-    def test_call(self, access_token, access_token_of_blocked_user, mocker):
+    def test_call(self, _request, access_token, access_token_of_blocked_user, mocker):
         middleware = JWTMiddleware(mocker.MagicMock(return_value=None))
-        request = mocker.MagicMock()
 
         with pytest.raises(AuthenticationFailed):
-            middleware(request)
+            middleware(_request)
 
-        request.COOKIES = {'access_token': access_token_of_blocked_user}
+        _request.COOKIES = {'access_token': access_token_of_blocked_user}
         with pytest.raises(PermissionDenied):
-            middleware(request)
+            middleware(_request)
 
-        request.COOKIES = {'access_token': access_token}
-        assert middleware(request) is None
+        _request.COOKIES = {'access_token': access_token}
+        assert middleware(_request) is None
