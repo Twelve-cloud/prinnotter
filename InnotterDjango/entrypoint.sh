2555078620f5fc24b5ef8d88cbb200a79838b398
Twelve <kana.suzucki@gmail.com>
2022-09-20 03:14:28 +0300

    fix: some fixes

--- InnotterDjango/entrypoint.sh
+++ InnotterDjango/entrypoint.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 poetry run python manage.py migrate
 poetry run python manage.py createsuperuser --noinput
-poetry run python manage.py runserver 0.0.0.0:8000
+poetry run python manage.py runserver 0.0.0.0:$PORT
