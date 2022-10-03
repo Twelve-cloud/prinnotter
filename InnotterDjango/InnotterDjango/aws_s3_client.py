344a32bcb11131de6131fcb0ca149a1c7b9ae5d1
twelve <kana.suzucki@gmail.com>
2022-10-03 15:40:04 +0300

    new: added new function that generate url for image in S3Client class

--- InnotterDjango/InnotterDjango/aws_s3_client.py
+++ InnotterDjango/InnotterDjango/aws_s3_client.py
@@ -1,13 +1,30 @@
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from InnotterDjango.aws_metaclass import AWSMeta
+from InnotterDjango.exceptions import BucketDoesNotExist
+from botocore.exceptions import ClientError
 from django.conf import settings
+import io
 
 
 class S3Client(metaclass=AWSMeta):
     service_name = 's3'
 
     @classmethod
-    def upload_file(cls, file_name: str, object_name: str) -> None:
-        response = cls.client.upload_file(file_name, settings.AWS_BUCKET_NAME, object_name)
+    def upload_file(cls, image: InMemoryUploadedFile, key: str) -> None:
+        try:
+            bfile = io.BytesIO(image.read())
+            cls.client.upload_fileobj(bfile, settings.AWS_BUCKET_NAME, key)
+        except ClientError:
+            raise BucketDoesNotExist()
 
-        if not response:
-            raise Exception(f'file "{file_name}" has been not uploaded')
+    @classmethod
+    def create_url(cls, key: str, expiration: int = 3600) -> str:
+        try:
+            url = cls.client.generate_presigned_url(
+                'get_object',
+                Params={'Bucket': settings.AWS_BUCKET_NAME, 'Key': key},
+                ExpiresIn=expiration
+            )
+            return url
+        except ClientError:
+            return BucketDoesNotExist()
